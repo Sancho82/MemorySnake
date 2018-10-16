c4ad01557bc8491fc4b7986dccb9a7691047a8f5
szabadee <kreativ.szabadi@gmail.com>
2018-10-16 17:06:02 +0200

    szabadi/stepper

--- table-objects.js
+++ table-objects.js
@@ -1,19 +1,50 @@
 //let readline = require('readline-sync');
-let pista = require('table');
+let table = require('table');
 let ctx = require('axel');
+let readlineSync = require('readline-sync');
+let keypress = require('keypress');
+const clear = require('console-clear');
 
 //ctx.clear();
 
+let coordinate = 0;
+
 const tableBoxes = [
-  {box1: 'X'},
-  {box2: ' '},
-  {box3: ' '}
+  {key: 'X'},
+  {key: ' '},
+  {key: ' '},
+  {key: ' '}
 ];
 
-let tableData = [];
+let tableDatas = [];
+let x = '';
 
-tableData.push([tableBoxes[0].box1, tableBoxes[1].box2, tableBoxes[2].box3]);
+tableDatas.push([tableBoxes[0].key, tableBoxes[1].key, tableBoxes[2].key]);
 
-let tableView = pista.table(tableData);
+let tableView = table.table(tableDatas);
 
 console.log(tableView);
+
+function stepperRight() {
+  coordinate++;
+  tableDatas = [];
+  x = tableBoxes[coordinate].key;
+  tableBoxes[coordinate].key = tableBoxes[coordinate + 1].key;
+  tableBoxes[coordinate + 1].key = x;
+  for (i = 0; i < tableBoxes.length; i++) {
+    tableDatas.push([tableBoxes[i].key]);
+  }
+  tableView = 0;
+  tableView = table.table(tableDatas);
+  clear();
+  console.log(tableView);
+}
+
+while (true) {
+  let location = readlineSync.question();
+  if (location === '[C') {
+    stepperRight();
+  } else {
+    break;
+  }
+}
