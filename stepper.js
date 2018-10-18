58f2f98aa6d4f11a30a44cf181f698e94f943ed6
savanyab <savanyab@gmail.com>
2018-10-18 10:52:33 +0200

    savanyab/stepper2

--- stepper.js
+++ stepper.js
@@ -14,7 +14,7 @@ const startingObjectGenerator = () => {
       startingObject[i].push({ element: '█' });
     }
   }
- // console.log(startingObject);
+  // console.log(startingObject);
 };
 
 const startingTableGenerator = () => {
@@ -24,47 +24,62 @@ const startingTableGenerator = () => {
       startingTable[i].push(startingObject[i][j].element);
     }
   }
-  console.log(table.table(startingTable));
+  //console.log(table.table(startingTable));
 };
 
 startingObjectGenerator();
 startingObject[0][0] = { element: "X" };
 startingTableGenerator()
 
+const moveUp = () => {
+  if (x != 0) {
+    [startingTable[x][y], startingTable[x - 1][y]] = [startingTable[x - 1][y], startingTable[x][y]];
+    x--;
+  }
+}
 
-let x =0;
+const moveDown = () => {
+  if (x != 6) {
+    [startingTable[x][y], startingTable[x + 1][y]] = [startingTable[x + 1][y], startingTable[x][y]];
+    x++;
+  }
+}
+
+const moveRight = () => {
+  if (y != 6) {
+    [startingTable[x][y + 1], startingTable[x][y]] = [startingTable[x][y], startingTable[x][y + 1]];
+    y++;
+  }
+}
+
+const moveLeft = () => {
+  if (y != 0) {
+    [startingTable[x][y - 1], startingTable[x][y]] = [startingTable[x][y], startingTable[x][y - 1]];
+    y--;
+  }
+}
+
+let x = 0;
 let y = 0;
 while (true) {
   clear.clear();
   console.log(table.table(startingTable));
   let direction = readline.question("?")
   switch (direction) {
-    case ('u'): 
-      if (x != 0) {
-        [startingTable[x][y], startingTable[x - 1][y]] = [startingTable[x - 1][y], startingTable[x][y]];
-        x--;     //array matching
-      }
+    case ('[A'):
+      moveUp();
       break;
-    case ('d'): 
-      if (x != 6) {
-        [startingTable[x][y], startingTable[x + 1][y]] = [startingTable[x + 1][y], startingTable[x][y]];
-        x++;
-      }
+    case ('[B'):
+      moveDown();
       break;
-    case ('r'): 
-      if (y != 6) {
-        [startingTable[x][y + 1], startingTable[x][y]] = [startingTable[x][y], startingTable[x][y + 1]];
-        y++;
-      }
+    case ('[C'):
+      moveRight();
       break;
-    case ('l'): 
-      if (y != 0) {
-        [startingTable[x][y - 1], startingTable[x][y]] = [startingTable[x][y], startingTable[x][y - 1]];
-        y--;
-      }
+    case ('[D'):
+      moveLeft();
       break;
     default:
       console.log("???");
       return;
-  } 
+  }
 }
